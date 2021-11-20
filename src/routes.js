47c2866482c023083e40295b7f6634b7a1837c37
kauecdev <kaue_cavalcante.cnt@outlook.com>
2021-11-20 01:21:03 -0300

    feat: created new route to send reset password email and reset password

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,22 @@
 const { Router } = require('express');
 const axios = require('axios');
 
+const sendResetPasswordEmail = require('./services/email/sendResetPasswordEmail');
+const resetPassword = require('./services/resetPassword');
+
 const router = Router();
 
 router.get('/news', (request, response) => {
     return getNews(request, response);
 });
+
+router.post('/forgot-password', (request, response) => {
+    return sendResetPasswordEmail(request, response);
+});
+
+router.post('/reset-password/:userId/:token', (request, response) => {
+    return resetPassword(request, response);
+})
 
 const getNews = async (request, response) => {
     const newsResponse = await axios.get(`https://newsapi.org/v2/everything?language=pt&q=musica&sortBy=publishedAt&apiKey=6de76939696f4bbfbf062edd5db3e562`)
